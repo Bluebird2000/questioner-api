cc22175770eabd869fa54b70dae2b1de5c560391
Bluebird2000 <olamilekanahmad7@gmail.com>
2019-01-03 21:21:24 +0100

    [finshes #162955983] Add / create rsvp for meetup if meetup record does exist

---
+++ api/controllers/rsvp.js
@@ -0,0 +1,51 @@
+const rsvps = [
+  {
+    id: 1,
+    meetup: 1,
+    user: 1,
+    topic: 'React summit',
+    response: 'yes',
+  },
+];
+const meetups = [
+  {
+    id: 1,
+    createdOn: new Date().getFullYear(),
+    location: 'Ajah',
+    topic: 'React summit',
+    happeningOn: '2019-01-03',
+    tags: ['nodejs', 'react', 'mongo', 'express'],
+  },
+  {
+    id: 2,
+    createdOn: new Date().getFullYear(),
+    location: 'Ojodu berger',
+    topic: 'Developer fest',
+    happeningOn: '2019-01-03',
+    tags: ['laravel', 'Django', 'python'],
+  },
+];
+
+exports.create_rsvp_response = (req, res) => {
+  const meetup = meetups.find(m => m.id === parseInt(req.params.id));
+  if (!meetup) {
+    res.status(404)
+      .send({
+        status: 404,
+        error: `Meetup with the given ID: ${req.params.id} does not exist`,
+      });
+    return;
+  }
+  const rsvp = {
+    id: rsvps.length + 1,
+    meetup: req.body.meetup,
+    user: req.body.user,
+    topic: req.body.topic,
+    response: req.body.response,
+  };
+  rsvps.push(rsvp);
+  res.status(200).send({
+    status: 200,
+    data: [rsvp],
+  });
+};
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import userRoutes from './api/routes/users';
 import meetupRoutes from './api/routes/meetups';
 import meetupQuestionRoutes from './api/routes/meetupquestions';
+import rsvpRoutes from './api/routes/rsvp';
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,5 +13,7 @@ app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/meetups', meetupRoutes);
 
 app.use('/api/v1/questions', meetupQuestionRoutes);
+
+app.use('/api/v1', rsvpRoutes);
 
 module.exports = app;
