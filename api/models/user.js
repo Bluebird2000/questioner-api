60c4eecdbdf92de1ed73fedb2be09f0fdda2d092
Bluebird2000 <olamilekanahmad7@gmail.com>
2019-01-10 16:26:25 +0100

    improve api validation requestion

--- api/models/user.js
+++ api/models/user.js
@@ -6,11 +6,10 @@ const userValidation = (qUsers) => {
     lastname: Joi.string().min(3).max(50).required(),
     othername: Joi.string().min(3).max(50).required(),
     email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-    phoneNumber: Joi.string().min(3).max(50).required(),
-    username: Joi.string().min(3).max(50).required(),
-    password: Joi.string().min(3).max(50).required(),
-    // isAdmin: Joi.string().required(),
+    phoneNumber: Joi.string().trim().regex(/^[0-9]{7,11}$/).required(),
+    username: Joi.string().min(3).max(16).required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).required(),
   };
   return Joi.validate(qUsers, userSchema);
 };
-module.exports = userValidation;
+export default userValidation;
