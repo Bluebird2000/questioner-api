732ef1e128e10b9bd4969e1a63529dba7f67284f
Bluebird2000 <olamilekanahmad7@gmail.com>
2019-01-16 09:29:49 +0100

    truncate database table before each test run

--- api/dbconfig/database.js
+++ api/dbconfig/database.js
@@ -3,10 +3,11 @@ import { Client } from 'pg';
 
 dotenv.config();
 
-const { DATABASE_URL, DATABASE_TEST_URL, NODE_ENV } = process.env;
+const { DATABASE_TEST_URL, DATABASE_URL, NODE_ENV } = process.env;
 
 if (NODE_ENV === 'development') {
   const connectionString = DATABASE_URL;
+  console.log(connectionString);
   const client = new Client({
     connectionString,
   });
@@ -21,22 +22,80 @@ if (NODE_ENV === 'development') {
 
   const userTableQuery = `DROP TABLE IF EXISTS users cascade;
         CREATE TABLE users(
-          id SERIAL PRIMARY KEY, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255), othername VARCHAR(255), email VARCHAR(225) UNIQUE NOT NULL, phoneNumber VARCHAR(255), username VARCHAR(255), password TEXT NOT NULL, isAdmin VARCHAR(255), created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW())`;
+          id SERIAL PRIMARY KEY,
+          firstname VARCHAR(255) NOT NULL, 
+          lastname VARCHAR(255), 
+          othername VARCHAR(255), 
+          email VARCHAR(225) UNIQUE NOT NULL, 
+          phoneNumber VARCHAR(255), 
+          username VARCHAR(255), 
+          password TEXT NOT NULL, 
+          isAdmin VARCHAR(255), 
+          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+          updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW())`;
 
   const createTestUser = `INSERT INTO users(
-          firstname, lastname, othername, email, phoneNumber, username, password, isAdmin, created_at, updated_at)
-          VALUES('test', 'tester', 'testest', 'tester@questioner.com', '08097012219', 'Bluebird2000', '$2a$10$HlG7pZklq8YqxvCmj6JNxuM9Gwuk/5cRzt4uARUNIJVqUNWlxBtQS', 'true', NOW(), NOW())`;
+          firstname, 
+          lastname, 
+          othername, 
+          email, 
+          phoneNumber, 
+          username, 
+          password, 
+          isAdmin, 
+          created_at, 
+          updated_at
+          )
+          VALUES(
+          'test', 
+          'tester', 
+          'testest', 
+          'tester@questioner.com', 
+          '08097012219', 
+          'Bluebird2000', 
+          '$2a$10$HlG7pZklq8YqxvCmj6JNxuM9Gwuk/5cRzt4uARUNIJVqUNWlxBtQS', 
+          'true', 
+          NOW(), 
+          NOW()
+          )`;
 
   const meetupTableQuery = `DROP TABLE IF EXISTS meetups cascade;
         CREATE TABLE meetups(
-          meetup_id SERIAL PRIMARY KEY, location VARCHAR(255) NOT NULL, topic VARCHAR(255) UNIQUE NOT NULL, happeningOn VARCHAR(255), created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW())`;
+          meetup_id SERIAL PRIMARY KEY, 
+          location VARCHAR(255) NOT NULL, 
+          topic VARCHAR(255) UNIQUE NOT NULL, 
+          happeningOn VARCHAR(255), 
+          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+          updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+          )`;
 
   const createTestMeetup = `INSERT INTO meetups(
-          location, topic, happeningOn, created_at, updated_at)
-          VALUES('Ojodu Berger', 'React summit', '2019-01-03', NOW(), NOW())`;
+          location, 
+          topic, 
+          happeningOn, 
+          created_at, 
+          updated_at
+          )
+          VALUES(
+          'Ojodu Berger', 
+          'React summit', 
+          '2019-01-03', NOW(), 
+          NOW()
+          )`;
   const createTestMeetup2 = `INSERT INTO meetups(
-          location, topic, happeningOn, created_at, updated_at)
-          VALUES('Epic Tower', 'Advancing Human potentials By investing in tomorrows leader', '2019-01-03', NOW(), NOW())`;
+          location, 
+          topic, 
+          happeningOn, 
+          created_at, 
+          updated_at
+          )
+          VALUES(
+          'Epic Tower', 
+          'Advancing Human potentials By investing in tomorrows leader', 
+          '2019-01-03', NOW(), 
+          NOW()
+          )`;
+
   const tableQuery = `${userTableQuery} ; ${createTestUser} ; ${meetupTableQuery} ; ${createTestMeetup2} ; ${createTestMeetup}`;
   client.query(tableQuery, (error) => {
     client.end();
@@ -44,7 +103,7 @@ if (NODE_ENV === 'development') {
       console.log(error.message);
       return;
     }
-    console.log('database development migration was successful');
+    console.log('database migration was successful for development server');
   });
 } else if (NODE_ENV === 'test') {
   const connectionString = DATABASE_TEST_URL;
@@ -57,25 +116,93 @@ if (NODE_ENV === 'development') {
       console.log(err.message);
       client.end();
     } else {
-      console.log('connection to test server established');
+      console.log('connection to test server successful');
     }
   });
 
   const userTableQuery = `DROP TABLE IF EXISTS users cascade;
         CREATE TABLE users(
-          id SERIAL PRIMARY KEY, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255), othername VARCHAR(255), email VARCHAR(225) UNIQUE NOT NULL, phoneNumber VARCHAR(255), username VARCHAR(255), password TEXT NOT NULL, isAdmin VARCHAR(255), created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW())`;
+          id SERIAL PRIMARY KEY,
+          firstname VARCHAR(255) NOT NULL, 
+          lastname VARCHAR(255), 
+          othername VARCHAR(255), 
+          email VARCHAR(225) UNIQUE NOT NULL, 
+          phoneNumber VARCHAR(255), 
+          username VARCHAR(255), 
+          password TEXT NOT NULL, 
+          isAdmin VARCHAR(255), 
+          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+          updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW())`;
 
   const createTestUser = `INSERT INTO users(
-          firstname, lastname, othername, email, phoneNumber, username, password, isAdmin, created_at, updated_at)
-          VALUES('test', 'tester', 'testest', 'tester@questioner.com', '08097012219', 'Bluebird2000', '$2a$10$HlG7pZklq8YqxvCmj6JNxuM9Gwuk/5cRzt4uARUNIJVqUNWlxBtQS', 'true', NOW(), NOW())`;
+          firstname, 
+          lastname, 
+          othername, 
+          email, 
+          phoneNumber, 
+          username, 
+          password, 
+          isAdmin, 
+          created_at, 
+          updated_at
+          )
+          VALUES(
+          'test', 
+          'tester', 
+          'testest', 
+          'tester@questioner.com', 
+          '08097012219', 
+          'Bluebird2000', 
+          '$2a$10$HlG7pZklq8YqxvCmj6JNxuM9Gwuk/5cRzt4uARUNIJVqUNWlxBtQS', 
+          'true', 
+          NOW(), 
+          NOW()
+          )`;
 
-  const tableQuery = `${userTableQuery} ; ${createTestUser}`;
+  const meetupTableQuery = `DROP TABLE IF EXISTS meetups cascade;
+        CREATE TABLE meetups(
+          meetup_id SERIAL PRIMARY KEY, 
+          location VARCHAR(255) NOT NULL, 
+          topic VARCHAR(255) UNIQUE NOT NULL, 
+          happeningOn VARCHAR(255), 
+          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+          updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+          )`;
+
+  const createTestMeetup = `INSERT INTO meetups(
+          location, 
+          topic, 
+          happeningOn, 
+          created_at, 
+          updated_at
+          )
+          VALUES(
+          'Ojodu Berger', 
+          'React summit', 
+          '2019-01-03', NOW(), 
+          NOW()
+          )`;
+  const createTestMeetup2 = `INSERT INTO meetups(
+          location, 
+          topic, 
+          happeningOn, 
+          created_at, 
+          updated_at
+          )
+          VALUES(
+          'Epic Tower', 
+          'Advancing Human potentials By investing in tomorrows leader', 
+          '2019-01-03', NOW(), 
+          NOW()
+          )`;
+
+  const tableQuery = `${userTableQuery} ; ${createTestUser} ; ${meetupTableQuery} ${createTestMeetup} ; ${createTestMeetup2}`;
   client.query(tableQuery, (error) => {
     client.end();
     if (error) {
       console.log(error.message);
       return;
     }
-    console.log('database test migration was successful');
+    console.log('database migration was successful for test server');
   });
 }
---
+++ api/dbconfig/truncate.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import { Client } from 'pg';
+
+dotenv.config();
+
+const connectionString = process.env.DATABASE_TEST_URL;
+const client = new Client({
+  connectionString,
+});
+console.log(connectionString);
+client.connect((err) => {
+  if (err) {
+    console.log(err.message);
+    client.end();
+  } else {
+    console.log('connection to test server established');
+  }
+});
+
+const truncateUserTableQuery = 'TRUNCATE TABLE users';
+
+const tableQuery = `${truncateUserTableQuery}`;
+client.query(tableQuery, (error) => {
+  client.end();
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+  console.log('Truncate tables before each test run', process.env.NODE_ENV);
+});
