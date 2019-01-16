68731cbe3838cf0a12beb1f7f6eb0a72d1618529
Bluebird2000 <olamilekanahmad7@gmail.com>
2019-01-16 16:30:13 +0100

    refactor app test suite

--- test/meetups.test.js
+++ test/meetups.test.js
@@ -18,3 +18,23 @@ describe('GET / All meetup records', () => {
       });
   });
 });
+
+describe('GET / single meetup', () => {
+  it('should return status code 200 if meetup record does exist', (done) => {
+    chai.request(app)
+      .get('/api/v1/meetups/1')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('should return status code 404 if meetup not found', (done) => {
+    chai.request(app)
+      .get('/api/v1/meetups/-1')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+});
